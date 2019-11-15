22239e057a2c6f5d47de8dd842dbef9e949a3627
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-11-15 01:01:15 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
-has_many  :tweets
+class User < ActiveRecord::Base
+  has_many  :tweets
 
 
   has_secure_password
@@ -10,3 +11,5 @@ has_many  :tweets
   def self.find_by_slug(slug)
     User.all.find{|user| user.slug == slug}
   end
+
+end
